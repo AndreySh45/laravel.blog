6cde303228616ab21cc773ddeaba7839183bbc65
AndreySh45 <and45@mail.ru>
2021-03-04 14:51:48 +0500

    Search

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,7 @@ Route::get('/',[PostController::class, "index"] )->name('home');
 Route::get('/article/{slug}',[PostController::class, "show"] )->name('posts.single');
 Route::get('/category/{slug}',[CategoryController::class, "show"] )->name('categories.single');
 Route::get('/tag/{slug}',[TagController::class, "show"] )->name('tags.single');
+Route::get('/search',[SearchController::class, "index"] )->name('search');
 
 Route::group(['prefix' => 'admin', 'namespace' => 'App\Http\Controllers\Admin', 'middleware' => 'admin'],
  function () {
